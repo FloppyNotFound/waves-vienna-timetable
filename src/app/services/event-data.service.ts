02eb66197543fdaa5a168b1b721912663033db5a
Felix Wagner <fwagner@mailbox.org>
2022-08-20 18:38:02 +0200

    fix: escape quotes

--- src/app/services/event-data.service.ts
+++ src/app/services/event-data.service.ts
@@ -59,6 +59,8 @@ export class EventDataService {
           d.excerpt = d.excerpt.replace('</p>', '');
           d.excerpt = d.excerpt.replace(/<img .*?>/g, '');
           d.excerpt = d.excerpt.replace('&#8230;', '');
+          d.excerpt = d.excerpt.replace('&#8220;', '"');
+          d.excerpt = d.excerpt.replace('&#8221;', '"');
           d.excerpt = d.excerpt.replace('&nbsp;', ' ');
         });
 
