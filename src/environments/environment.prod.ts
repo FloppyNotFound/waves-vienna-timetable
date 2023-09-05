73e551e0c385c9d50037bfc5f7c125a3252ef1b7
FloppyNotFound <fwagner@mailbox.org>
2023-09-05 19:24:06 +0200

    Feature/spotify data (#2)
    * feat(spotify): add spotify link, where available
    
    * feat(spotify): add image and follower amount

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  spotify: {
+    clientId: '7f8e0e82b5c94f9393ba56b05cd8b4eb',
+    clientSecret: '1178541fc25043d59b977e547eb8c8bc'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  spotify: {
+    clientId: '7f8e0e82b5c94f9393ba56b05cd8b4eb',
+    clientSecret: '1178541fc25043d59b977e547eb8c8bc'
+  }
 };
 
 /*
